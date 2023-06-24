f4dcd69677a0dd470a6c7976fcdb5d2dc84a04e2
garethduncandev <gareth.duncan87@gmail.com>
2023-06-24 16:30:40 +0100

    removed dotenv

--- src/cdk/bin/app.ts
+++ src/cdk/bin/app.ts
@@ -1,15 +1,10 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import * as dotenv from 'dotenv';
 
 import { ApplicationStack } from '../lib/application-stack';
 
-// load environment variables if .env file is present
-dotenv.config();
-
 const app = new cdk.App();
 
-// for each environment create 2 stacks, one for blue and one for green
 new ApplicationStack(app, 'garethduncandev-dev', {
   domain: 'garethduncan.dev',
   subDomain: 'dev',
