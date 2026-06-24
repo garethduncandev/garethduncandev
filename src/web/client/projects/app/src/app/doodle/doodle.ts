1d821da003f525afddcd4b40ac7fb6135aaed10a
garethduncandev <2130124+garethduncandev@users.noreply.github.com>
2026-06-24 20:51:38 +0100

    Improved doodles

--- src/web/client/projects/app/src/app/doodle/doodle.ts
+++ src/web/client/projects/app/src/app/doodle/doodle.ts
@@ -2,20 +2,18 @@ import { Component, signal } from '@angular/core';
 import { BlockDoodleComponent } from './block/block';
 import { HashDoodleComponent } from './hash/hash';
 import { OutlineDoodleComponent } from './outline/outline';
-import { SlimDoodleComponent } from './slim/slim';
 
 const DOODLE_COUNT = 4;
 
 @Component({
   selector: 'app-doodle',
-  imports: [BlockDoodleComponent, HashDoodleComponent, OutlineDoodleComponent, SlimDoodleComponent],
+  imports: [BlockDoodleComponent, HashDoodleComponent, OutlineDoodleComponent],
   template: `
     <div (click)="next()" class="cursor-pointer select-none flex items-center justify-center aspect-[3/1] [container-type:inline-size]">
       @switch (index()) {
         @case (0) { <app-block-doodle /> }
         @case (1) { <app-outline-doodle /> }
         @case (2) { <app-hash-doodle /> }
-        @case (3) { <app-slim-doodle /> }
       }
     </div>
   `,
--- src/web/client/projects/app/src/app/doodle/slim/slim.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-slim-doodle',
-  templateUrl: './slim.html',
-  host: { class: 'contents' },
-})
-export class SlimDoodleComponent {}
