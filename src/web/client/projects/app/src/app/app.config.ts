3cb5d37dbed3d2c3973d1cafcd3b33af50f4586f
garethduncandev <2130124+garethduncandev@users.noreply.github.com>
2026-08-13 22:08:42 +0100

    PWA

--- src/web/client/projects/app/src/app/app.config.ts
+++ src/web/client/projects/app/src/app/app.config.ts
@@ -1,13 +1,15 @@
 import {
   ApplicationConfig,
+  isDevMode,
   provideAppInitializer,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { appSettingsInitializerFactory } from './app-settings.initializer';
 
 export const appConfig: ApplicationConfig = {
@@ -17,5 +19,9 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideHttpClient(withFetch()),
     provideAppInitializer(appSettingsInitializerFactory),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
